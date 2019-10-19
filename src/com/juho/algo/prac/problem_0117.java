9d08b1f4d0807c4817410df62a4eb5af31e4ed58
leejooho77 <leejooho@uga.edu>
2019-10-19 16:55:08 -0400

    added problem_0117

---
+++ src/com/juho/algo/prac/problem_0117.java
@@ -0,0 +1,32 @@
+package com.juho.algo.prac;
+
+/**
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ *
+ * Example 1:
+ * Input: [1,2,3], Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ *
+ * Example 2:
+ * Input: [4,3,2,1], Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ *
+ * @author Juho Lee
+ * @since Oct 19, 2019
+ */
+public class problem_0117 {
+
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(++digits[i] == 10) {
+                digits[i] = 0;
+            } else return digits;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+
+}
